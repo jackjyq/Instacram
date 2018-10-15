e743d29a34b60541bd96d7dc51aa1074af480d5c
sommd <d.j.s.1050@gmail.com>
2018-10-16 00:52:59 +1100

    Improve error message if JSON object is missing keys

--- backend/util/globals.py
+++ backend/util/globals.py
@@ -3,10 +3,12 @@ from app import db
 from flask_restplus import Resource, abort, reqparse, fields
 
 def unpack(j,*args,**kargs):
-    r = [j.get(arg,None) for arg in args]
     if kargs.get("required",True):
-        [abort(kargs.get("error",400)) for e in r if e == None]
-    return r
+        not_found = [arg for arg in args if arg not in j]
+        if not_found:
+            expected = ", ".join(map(str, not_found))
+            abort(kargs.get("error",400), "Expected request object to contain: " + expected)
+    return [j[arg] for arg in args]
 
 def gen_token():
     token = secrets.token_hex(32)
